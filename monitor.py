bb0e0db0e3552eb0c60e4701d540aea145a5f467
sebastianvargasy <64619819+sebastianvargasy@users.noreply.github.com>
2023-02-21 00:47:16 -0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,38 +1,59 @@
-import time
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime, timedelta
+import pytz
 
 # Configuración de página
-st.set_page_config(page_title="Monitoreo de www.sochisi.cl", page_icon="📈", layout="wide")
+st.set_page_config(page_title="Monitor Web", page_icon="🕸️", layout="wide")
+
+# Configuración de hora local
+local_tz = pytz.timezone('America/Santiago')
+
+# URL del sitio web a monitorear
+url = "https://www.sochisi.cl"
 
 # Encabezado de la página
-st.markdown("<h1 style='text-align: center;'>Monitoreo de www.sochisi.cl</h1>", unsafe_allow_html=True)
-
-# Tabla para mostrar los resultados
+st.markdown("<h1 style='text-align: center;'>Monitor Web</h1>", unsafe_allow_html=True)
 st.write(" ")
-st.write("### Estado del sitio")
-table = st.table([])
+st.write(f"Monitoreando el sitio web {url} cada 5 segundos...")
+
+# Configuración de tabla
+col1, col2 = st.beta_columns(2)
+with col1:
+    st.write("Hora")
+with col2:
+    st.write("Estado")
 
 # Monitoreo del sitio web
 while True:
-    # Hacemos la petición al sitio web
-    url = "http://www.sochisi.cl"
-    response = requests.get(url)
+    try:
+        # Realizamos la petición GET al sitio web
+        response = requests.get(url)
+        response.raise_for_status()
 
-    # Obtenemos el estado del sitio web
-    if response.status_code == 200:
-        status = "En línea"
-    else:
-        status = "Fuera de línea"
+        # Analizamos el contenido HTML de la respuesta
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Obtenemos la fecha y hora actual
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        # Obtenemos la hora actual en Santiago de Chile
+        current_time = datetime.now(local_tz).strftime('%Y-%m-%d %H:%M:%S')
 
-    # Agregamos los resultados a la tabla
-    data = [[status, now]]
-    table.add_rows(data)
+        # Agregamos la fila a la tabla
+        with col1:
+            st.write(current_time)
+        with col2:
+            st.write("🟢 En línea")
 
-    # Esperamos 5 segundos antes de hacer la siguiente petición
+    except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError):
+        # Obtenemos la hora actual en Santiago de Chile
+        current_time = datetime.now(local_tz).strftime('%Y-%m-%d %H:%M:%S')
+
+        # Agregamos la fila a la tabla
+        with col1:
+            st.write(current_time)
+        with col2:
+            st.write("🔴 Fuera de línea")
+
+    # Esperamos 5 segundos antes de realizar la siguiente petición
     time.sleep(5)
+
